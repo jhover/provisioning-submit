6ea456191bf9dd15db98882d3865b52a0cb24f2c
Jose Caballero <jcaballero@bnl.gov>
2016-07-26 17:42:54 -0400

    new code organization for the nova client

--- bin/get_nova_vm_api.py
+++ bin/get_nova_vm_api.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python 
 
-#################################################################
-#   FIXME !!!
-#   the split into classes MyNova and MyNovaUserInterface
-#   was not done properly
-#   the core class MyNova should not have any call to the 
-#   UserInterface class
-#   it should be the opposite: the UI class calls the core class
-#################################################################
-
-
 #
 # documentation on the python API:
 #       http://docs.openstack.org/developer/python-novaclient/ref/v2/
@@ -41,7 +31,6 @@
 #
 
 import commands
-import logging
 import os
 import pwd
 import sys
@@ -49,115 +38,15 @@ import time
 
 # FIXME: 
 # what to do if the import fails?
-from novaclient import client
-
-
-class MyNovaUserInterface(object):
-
-    def __init__(self, verbosity):
-
-        class BColors:
-            #
-            # WARNING:
-            # problem with colors is that if we add a handler to the logger
-            # to print messages to a file, 
-            # the file get filled with nasty mark symbols
-            #
-            HEADER = '\033[95m'
-            OKBLUE = '\033[94m'
-            OKGREEN = '\033[92m'
-            WARNING = '\033[93m'
-            FAIL = '\033[91m'
-            ENDC = '\033[0m'
-            BOLD = '\033[1m'
-            UNDERLINE = '\033[4m'
-
-        self.bcolors = BColors()
-        self.verbosity = verbosity
-        self._getlogger()
-
-    def _getlogger(self):
-
-        self.log = logging.getLogger('userinterface')
-        logStream = logging.StreamHandler()
-        #FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
-        FORMAT='%(message)s'
-        formatter = logging.Formatter(FORMAT)
-        formatter.converter = time.gmtime  # to convert timestamps to UTC
-        logStream.setFormatter(formatter)
-        self.log.addHandler(logStream)
-        self.log.setLevel(self.verbosity)
-
-
-    def debug(self, msg):
-        self.log.debug(msg)
-
-    def info(self, msg):
-        self.log.info(msg)
-
-    def warning(self, msg):
-        self.log.warning(msg)
-
-    def error(self, msg):
-        self.log.error(msg)
-
-    def critical(self, msg):
-        self.log.critical(msg)
-
-
-    def select_image(self, list_images):
-        return self._select_from_list(list_images, "VM images")
-
-    def select_flavor(self, list_flavors):
-        return self._select_from_list(list_flavors, "image flavors")
-
-    def select_delete(self, list_servers):
-        return self._select_from_list(list_servers, "VM instances (servers) currently running")
-    
-
-    def select_name(self, image_name):
-
-        username = pwd.getpwuid( os.getuid() )[ 0 ]
-        date = time.strftime('%y%m%d')
-        default_vm_name = "%s-%s-%s" %(image_name, username, date)
-        vm_name = raw_input("Type a name for the VM instance (or hit ENTER for suggested name: %s) " %default_vm_name )
-        if not vm_name:
-            vm_name = default_vm_name
-        return vm_name
-
-
-    def create_final_message(self, ip, vm_id):
-
-        self.log.info("now you can log into your new VM with command:")
-        self.log.info("     ssh root@%s" %ip.ip)
-        self.log.info("")
-        self.log.info("when finished, delete the VM with commands:")
-        self.log.info("     nova stop %s" %vm_id)
-        self.log.info("     nova delete %s" %vm_id)
-        self.log.info("or running this script with 'delete' option")
-        self.log.info("")
-
-
-    def _select_from_list(self, list_items, item_type):
-        """
-        generic method to display a list of options 
-        on the stdout, 
-        and let the user to pick one by index number
-        """
-
-        self.log.info("List of available %s:" %item_type)
-        for i in range(len(list_items)):
-            self.log.info("    %s%s %s%s : %s%s" %(self.bcolors.BOLD, self.bcolors.FAIL, i+1, self.bcolors.OKBLUE, list_items[i].name, self.bcolors.ENDC))
-        index = raw_input("Pick one by typing the index number: ")
-        index = int(index)
-        return index
+from novaclient import client as novaclient
 
 
 
+# =========================================================================
 
-class MyImage:
+class Image:
+
     def __init__(self, id, name, image):
-        
         self.id = id
         self.name = name
         self.image = image
@@ -170,197 +59,280 @@ class MyImage:
         else:
             return 0
 
+# =========================================================================
 
-class MyNova:
+class NovaCore:
 
-    def __init__(self):
+    def __init__(self, version, username, password, project_id, auth_url):
 
-        self._parseopts()
-        self._getlogger()
-        
-        try:
-            self._setenvironment()
-        except KeyError, k:
-            self.log.critical('mandatory variable %s is not defined in the environment', k)
-            raise Exception
+        self.VERSION = version
+        self.USERNAME = username
+        self.PASSWORD = password
+        self.PROJECT_ID = project_id
+        self.AUTH_URL = auth_url
 
-        self.nova = client.Client(self.VERSION, self.USERNAME, self.PASSWORD, self.PROJECT_ID, self.AUTH_URL)
-
-        self.ui = MyNovaUserInterface(self.verbosity)
+        self.client = novaclient.Client(self.VERSION, self.USERNAME, self.PASSWORD, self.PROJECT_ID, self.AUTH_URL)
 
 
-    def _parseopts(self):
-        """
-        TO BE IMPLEMENTED !!!
-        """
-        # FIXME
-        self.verbosity = logging.DEBUG
-
-
-    def _getlogger(self):
-        self.log = logging.getLogger('mynova')
-        logStream = logging.StreamHandler()
-        #FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
-        FORMAT='%(message)s'
-        formatter = logging.Formatter(FORMAT)
-        formatter.converter = time.gmtime  # to convert timestamps to UTC
-        logStream.setFormatter(formatter)
-        self.log.addHandler(logStream)
-        self.log.setLevel(self.verbosity)
-
-
-    def _setenvironment(self):
-
-        self.VERSION = "2"
-        self.USERNAME = os.environ['OS_USERNAME']
-        self.PASSWORD = os.environ['OS_PASSWORD']
-        self.PROJECT_ID = os.environ['OS_TENANT_NAME']
-        self.AUTH_URL = os.environ['OS_AUTH_URL']
-
-    def usage(self):
-
-        self.log.info('')
-        self.log.info('Tool to facilitate instantiating a VM with nova.')
-        self.log.info('Usage:')
-        self.log.info('')
-        self.log.info('$ python get_nova_vm_api.py create|delete')
-        self.log.info('')
-
-
-    def create(self):
-
-        self._get_vm_name()
-        self._get_image_id()
-        self._get_ip()
-        self._print_create_final_message()
-
-
-    def _get_list_images(self):
-
+    def get_list_images(self):
+    
         list_images = []
-
-        for image in self.nova.images.list():
+    
+        for image in self.client.images.list():
             if image.status == "ACTIVE":
-                list_images.append( MyImage(image.id, image.name, image) )
-
+                list_images.append( Image(image.id, image.name, image) )
+    
         list_images.sort()
         return list_images
 
-    def _set_iamge_properties(self, image):
-        # FIXME
-        # this method is most probably not needed
 
-        self.image_name = image.name
-        self.image_id = image.id
-        self.image = image.image
+    def get_list_flavors(self):
+        return self.client.flavors.list()
 
 
-    def _get_vm_name(self):
-
-        list_images = self._get_list_images()
-
-        index = self.ui.select_image(list_images)
-
-        image = list_images[index-1]
-        
-        self.vm_name = self.ui.select_name(self.image_name)
+    def get_flavor(self, flavor_name):
+        return self.client.flavors.find(name=flavor_name)
 
 
-    def _get_image_id(self):
-
-        self.ui.info("Instantiating VM %s ... (it may take a few seconds)" %self.vm_name)
-
-        #
-        #   >>> nova.flavors.list()
-        #   [<Flavor: m1.tiny>, <Flavor: m1.small>, <Flavor: m1.medium>, <Flavor: m1.large>, <Flavor: m1.xlarge>, <Flavor: m1.xlarge.160>, <Flavor: m1.2xlarge.750>, <Flavor: m1.2xlarge.500>, <Flavor: m1.2xlarge.700>]
-        #
-        #   >>> f = nova.flavors.list()[0]
-        #   >>> print f
-        #   <Flavor: m1.tiny>
-        #
-        #   >>> print f.name
-        #   m1.tiny
-        #
-        #   >>> print f.__dict__
-        #   {'name': u'm1.tiny', 'links': [{u'href': u'http://192.153.161.7:8774/v2/a629decc3bc8411a83cc210326db829c/flavors/1', u'rel': u'self'}, {u'href': u'http://192.153.161.7:8774/a629decc3bc8411a83cc210326db829c/flavors/1', u'rel': u'bookmark'}], 'ram': 512, 'vcpus': 1, 'id': u'1', 'OS-FLV-DISABLED:disabled': False, 'manager': <novaclient.v1_1.flavors.FlavorManager object at 0x10fced0>, 'swap': u'', 'os-flavor-access:is_public': True, 'rxtx_factor': 1.0, '_info': {u'name': u'm1.tiny', u'links': [{u'href': u'http://192.153.161.7:8774/v2/a629decc3bc8411a83cc210326db829c/flavors/1', u'rel': u'self'}, {u'href': u'http://192.153.161.7:8774/a629decc3bc8411a83cc210326db829c/flavors/1', u'rel': u'bookmark'}], u'ram': 512, u'OS-FLV-DISABLED:disabled': False, u'vcpus': 1, u'swap': u'', u'os-flavor-access:is_public': True, u'rxtx_factor': 1.0, u'OS-FLV-EXT-DATA:ephemeral': 0, u'disk': 1, u'id': u'1'}, 'disk': 1, 'OS-FLV-EXT-DATA:ephemeral': 0, '_loaded': True}
-        #   
-
-        list_flavors = self.nova.flavors.list()
-        index = self.ui.select_flavor(list_flavors)
-
-        name = list_flavors[index-1].name
-        #flavor = self.nova.flavors.find(name='m1.medium')
-        # FIXME:
-        #   make 'm1.medium' the default
-        flavor = self.nova.flavors.find(name=name)
-        self.server = self.nova.servers.create(self.vm_name, self.image, flavor=flavor)
-        #print ">>>>>>>>>>>>>>>>>"
-        #print dir(self.server)
-        #print
-        #print self.server.__dict__
-        #print ">>>>>>>>>>>>>>>>>"
-       
-        self.ui.info('Instantiating VM... (this step will take a few seconds)')
- 
-        while True:
-            self.server = self.nova.servers.find(name=self.vm_name)
-            status = self.server.status
-            power = int(self.server.__dict__['OS-EXT-STS:power_state'])
-            
-            if status == "ACTIVE" and power == 1:
-                self.vm_id = self.server.id
-                break
-         
-            time.sleep(1)
-
-        #print ">>>>>>>>>>>>>>>>>"
-        #print dir(self.server)
-        #print
-        #print self.server.__dict__
-        #print ">>>>>>>>>>>>>>>>>"
-        
-        self.ui.info("VM %s instantiated, with ID %s" %(self.vm_name, self.vm_id))
-       
-
-    def _get_ip(self):
-
-        self._get_floating_ip() 
-        self._get_fixed_ip()
-        
-
-    def _get_floating_ip(self): 
-        
-        list_floating_ips = self.nova.floating_ips.list()
-        # search for the first available IP not yet picked up
+    def next_free_floating_ip(self):
+        '''
+        search for the first available IP not yet picked up
+        '''
+        list_floating_ips = self.client.floating_ips.list()
         for ip in list_floating_ips:
             if not ip.fixed_ip:
                 self.ip = ip
                 break
 
+
+    def create_server(self, vm_name, image, flavor):
+
+        self.client.servers.create(vm_name, image, flavor=flavor)
+        while True:
+            server = self.get_server(vm_name)
+            status = server.status
+            power = int(server.__dict__['OS-EXT-STS:power_state'])
+            if status == "ACTIVE" and power == 1:
+                break
+            time.sleep(1)
+        return server
+
+
+    def get_server(self, vm_name):
+        return self.client.servers.find(name=vm_name)
+
+
+    def set_fixed_ip(self, server):
+
+        floating_ip = self.next_floating_ip() 
+        return self._get_fixed_ip(server, floating_ip)
+     
+
+    def next_floating_ip(self): 
+     
+        list_floating_ips = self.client.floating_ips.list()
+        # search for the first available IP not yet picked up
+        for floating_ip in list_floating_ips:
+            if not floating_ip.fixed_ip:
+                break
+        return floating_ip
+
     
-    def _get_fixed_ip(self): 
+    def _get_fixed_ip(self, server, floating_ip): 
 
-        self.nova.servers.add_floating_ip(self.server, self.ip.ip)
-        self.ip = self.nova.floating_ips.find(ip=self.ip.ip)
-        
+        self.client.servers.add_floating_ip(server, floating_ip.ip)
+        fixed_ip = self.client.floating_ips.find(ip=floating_ip.ip)
+        return fixed_ip
 
-    def _print_create_final_message(self):
-        self.ui.create_final_message(self.ip, self.vm_id)
+
+    def get_list_servers(self):
+        return self.client.servers.list()
+
+    def delete_server(self, server):
+        server.stop()
+        server.delete()
+
+    
+
+
+# =========================================================================
+   
+class NovaCLI:
+
+    def __init__(self):
+    
+        try:
+            self._setenvironment()
+        except KeyError, k:
+            print('mandatory variable %s is not defined in the environment', k)
+            raise Exception
+        self.core = NovaCore(self.VERSION, self.USERNAME, self.PASSWORD, self.PROJECT_ID, self.AUTH_URL)
+
+        class BColors:
+            HEADER = '\033[95m'
+            OKBLUE = '\033[94m'
+            OKGREEN = '\033[92m'
+            WARNING = '\033[93m'
+            FAIL = '\033[91m'
+            ENDC = '\033[0m'
+            BOLD = '\033[1m'
+            UNDERLINE = '\033[4m'
+        self.bcolors = BColors()
+
+
+    def _setenvironment(self):
+
+        self.VERSION = "2" 
+        self.USERNAME = os.environ['OS_USERNAME']
+        self.PASSWORD = os.environ['OS_PASSWORD']
+        self.PROJECT_ID = os.environ['OS_TENANT_NAME']
+        self.AUTH_URL = os.environ['OS_AUTH_URL']
+
+
+    # -------------------------------------------------------------------------
+    #       public interface
+    # -------------------------------------------------------------------------
+
+
+    def usage(self):
+
+        print('')
+        print('Tool to facilitate instantiating a VM with nova.')
+        print('Usage:')
+        print('')
+        print('$ python get_nova_vm_api.py create|delete')
+        print('')
+
+
+    def create(self):
+    
+        self.image = self._set_image()
+        self.vm_name = self._set_vm_name()
+        self.flavor = self._set_flavor()
+        self.server = self._create()
+        self.fixed_ip = self._set_ip()
+        self._print_login_message()
+
+
+
 
 
     def delete(self):
-
-        list_servers = self.nova.servers.list()
-        index = self.ui.select_delete(list_servers)
+    
+        list_servers = self.core.get_list_servers()
+        index = self._select_server_to_delete_from_list(list_servers)
         server = list_servers[index-1]
-        self.ui.info("Deleting VM instance with name %s ..." %server.name)
-        server.stop()
-        server.delete()
-        self.ui.info("VM instance with name %s deleted" %server.name)
+        print("Deleting VM instance with name %s ..." %server.name)
+        self.core.delete_server(server)
+        print("VM instance with name %s deleted" %server.name)
+
+
+
+
+
+    # -------------------------------------------------------------------------
+
+
+    def _set_image(self):
+        list_images = self.core.get_list_images()
+        index = self._select_image_from_list(list_images)
+        image = list_images[index-1]
+        return image
+
+
+    def _set_vm_name(self):
+
+        username = pwd.getpwuid( os.getuid() )[ 0 ] 
+        date = time.strftime('%y%m%d')
+        default_vm_name = "%s-%s-%s" %(self.image.name, username, date)
+        vm_name = raw_input("Type a name for the VM instance (or hit ENTER for suggested name: %s) " %default_vm_name )
+        if not vm_name:
+            vm_name = default_vm_name
+        return vm_name
+
+
+    def _set_flavor(self):
+
+        #flavor = self.nova.flavors.find(name='m1.medium')
+        # FIXME:
+        #   make 'm1.medium' the default
+        list_flavors = self.core.get_list_flavors()
+        index = self._select_flavor_from_list(list_flavors)
+        flavor_name = list_flavors[index-1].name
+        flavor = self.core.get_flavor(flavor_name)
+        return flavor
+
+
+    def _select_image_from_list(self, list_images):
+        return self._select_from_list(list_images, "VM images")
+
+    
+    def _select_flavor_from_list(self, list_flavors):
+        return self._select_from_list(list_flavors, "image flavors")
+
+    
+    def _select_server_to_delete_from_list(self, list_servers):
+        return self._select_from_list(list_servers, "VM instances (servers) currently running")
+
+
+    def _select_from_list(self, list_items, item_type):
+        """
+        generic method to display a list of options 
+        on the stdout, 
+        and let the user to pick one by index number
+        """
+
+        print("List of available %s:" %item_type)
+        for i in range(len(list_items)):
+            print("    %s%s %s%s : %s%s" %(self.bcolors.BOLD, self.bcolors.FAIL, i+1, self.bcolors.OKBLUE, list_items[i].name, self.bcolors.ENDC))
+        index = raw_input("Pick one by typing the index number: ")
+        index = int(index)
+        return index
+
+
+    def _create(self):
+
+        print("Instantiating VM %s ... (it may take a few seconds)" %self.vm_name)
+        server = self.core.create_server(self.vm_name, self.image, flavor=self.flavor)
+        self.server_id = server.id
+        print("VM %s instantiated, with ID %s" %(self.vm_name, self.server_id))
+        return server
+
+
+    def _set_ip(self):
+
+            return self.core.set_fixed_ip(self.server)
+
+
+    def _print_login_message(self):
+
+        print("now you can log into your new VM with command:")
+        print("     ssh root@%s" %self.fixed_ip.ip)
+        print("")
+        print("when finished, delete the VM with commands:")
+        print("     nova floating-ip-disassociate %s %s" %(self.server_id, self.fixed_ip.ip))
+        print("     nova stop %s" %self.server_id)
+        print("     nova delete %s" %self.server_id)
+        print("or running this script with 'delete' option")
+        print("")
+
+
+
+
+
+
+
+
+
+
+
 
 
     
-    
+
+
+ 
+# =========================================================================
 
 if __name__ == '__main__':
 
@@ -368,19 +340,19 @@ if __name__ == '__main__':
     # this needs to be done properly, with argparse, getopt, or similar.
 
     try:
-        mynova = MyNova()
+        cli = NovaCLI()
     except:
         # FIXME
         pass
         
     if len(sys.argv) != 2:
-        mynova.usage()
+        cli.usage()
         sys.exit()    
     
     if sys.argv[1] == 'create':
-        mynova.create()
+        cli.create()
     elif sys.argv[1] == 'delete':
-        mynova.delete()
+        cli.delete()
     else:
         #FIXME
         sys.exit()    
